af0c4b1f0e0d92bbdc9316d2b599ba91c3206573
henrique15775 <lhdeveloper137@gmail.com>
2023-02-21 20:07:06 -0300

    first commit

---
+++ src/main/java/instituto/pedro/alcantara/com/pedroAlcantaraDeclaracoes/PedroAlcantaraDeclaracoesApplication.java
@@ -0,0 +1,13 @@
+package instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedroAlcantaraDeclaracoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedroAlcantaraDeclaracoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/instituto/pedro/alcantara/com/pedroAlcantaraDeclaracoes/PedroAlcantaraDeclaracoesApplicationTests.java
@@ -0,0 +1,13 @@
+package instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedroAlcantaraDeclaracoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
