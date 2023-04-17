954d7a136737b05f1b6fc112a29995fea3692042
Luís Henrique <luis.freire@assert.ifpb.edu.br>
2023-04-17 15:17:11 -0300

    corrigir saves

--- src/main/java/instituto/pedro/alcantara/com/pedroAlcantaraDeclaracoes/service/PeriodoLetivoService.java
+++ src/main/java/instituto/pedro/alcantara/com/pedroAlcantaraDeclaracoes/service/PeriodoLetivoService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes.controller.dto.PeriodoLetivoDTO;
+import instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes.model.Instituicao;
 import instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes.model.PeriodoLetivo;
 import instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes.repository.InstituicaoRepository;
 import instituto.pedro.alcantara.com.pedroAlcantaraDeclaracoes.repository.PeriodoLetivoRepository;
@@ -22,14 +23,14 @@ public class PeriodoLetivoService {
     @Autowired
     private InstituicaoRepository instituicaoRepository;
 
-    public void save(@Valid PeriodoLetivo p) {
+    public void save(@Valid PeriodoLetivo p) throws Exception {
         periodoLetivoRepository.save(p);
-        instituicaoRepository.findAll().forEach(i -> {
-            List<PeriodoLetivo> periodosInstituicao = i.getPeriodos();
-            periodosInstituicao.add(p);
-            i.setPeriodos(periodosInstituicao);
-            i.setPeriodoAtual(p);
-        });
+        Instituicao i = instituicaoRepository.findById(p.getInstituicao().getId())
+            .orElseThrow(() -> new Exception("Deu ruim asoidasnoiads"));
+        List<PeriodoLetivo> periodosInstituicao = i.getPeriodos();
+        periodosInstituicao.add(p);
+        i.setPeriodos(periodosInstituicao);
+        i.setPeriodoAtual(p);
     }
 
     public Collection<PeriodoLetivoDTO> getAll() {
