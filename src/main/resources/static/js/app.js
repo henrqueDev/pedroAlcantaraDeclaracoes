6b8ec56ec8c10d41eff505d6d51f92c6f1626a93
gabebritto <gabrielsouto16@gmail.com>
2023-05-02 23:06:32 -0300

    add index page + create footer in layout

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,13 @@
+const Toast = Swal.mixin({
+    icon: 'success',
+    title: 'Enviado com Sucesso!',
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
